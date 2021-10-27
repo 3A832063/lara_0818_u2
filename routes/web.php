f0624a2939723b2231274e40c7f10b0da379d296
LIN GUAN-YU <kib7410@gmail.com>
2021-10-27 11:33:16 +0800

    '練習3:傳遞資料至view顯示'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,5 @@ Route::get('home', function (){
 */
 /*使用controller來回傳home頁面*/
 Route::get('home',[HomeController::class,"index"])->name("home.index");
+
+Route::get('hello/{name?}',[HelloController::class,"index"])->name("hello.index");
